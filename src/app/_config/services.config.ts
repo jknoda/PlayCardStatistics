09866882f99f40fa8fc5dfd7ab65e4f0b052cdf8
Jorge Noda <jknoda@hotmail.com>
2021-10-30 21:41:45 -0300

    versao 4.2

--- src/app/_config/services.config.ts
+++ src/app/_config/services.config.ts
@@ -1,5 +1,5 @@
 export class ServiceConfig {
-    public static VERSION: string = "Versão 4.1";
+    public static VERSION: string = "Versão 4.2";
     public static API_ENDPOINT: string = "https://playcardservice.herokuapp.com";
     //public static API_ENDPOINT: string = "http://localhost:3003";
     public static APIPY_ENDPOINT: string = "https://pyplaycards.herokuapp.com";
